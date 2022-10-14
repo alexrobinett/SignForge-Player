8c06db7eec48726b18fe5e4366e697da43c0dfc7
robinetta <alxr19@gmail.com>
2022-10-14 12:51:44 -0400

    trying new solution

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let animation = bodymovin.loadAnimation({
+    container: document.getElementById("anim"),
+    rederer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'JS/data.json',
+
+})
