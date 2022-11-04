39287d51d8ef95c839171967b38b3aef8c560c87
robinetta <alxr19@gmail.com>
2022-11-03 22:16:24 -0400

    fixed CSS bugs

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-let animation = bodymovin.loadAnimation({
-    container: document.getElementById("anim"),
-    rederer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'JS/data.json',
-
-})
